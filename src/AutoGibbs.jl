b1b6d9b1a68dd150c156d63099eb0605b781f3a1
Philipp Gabler <phipsgabler@users.noreply.github.com>
2020-03-02 17:57:40 +0100

    Implement very simple Turing fake model

--- src/AutoGibbs.jl
+++ src/AutoGibbs.jl
@@ -1,5 +1,57 @@
 module AutoGibbs
 
-greet() = print("Hello World!")
+using Distributions
+using DynamicPPL
+import AbstractMCMC
+
+abstract type AbstractEvaluator end
+
+
+struct SimpleModel{F} <: AbstractMCMC.AbstractModel
+    f::F
+end
+
+(model::SimpleModel)(data...) = function evaluate(evaluator::AbstractEvaluator)
+    return model.f(evaluator, data...)
+end
+
+
+# sampling from prior: record all latent variables and their logpdf
+struct Sample <: AbstractEvaluator end
+
+init_state(::Sample) = (logp = Ref(0.0), latent = Dict{VarName, Tuple{<:Real, Float64}}())
+
+function assume!(::Sample, state, vn, dist)
+    r = rand(dist)
+    ℓ = logpdf(dist, r)
+    push!(state.latent, vn => (r, ℓ))
+    state.logp[] += ℓ
+    return r, state
+end
+
+function observe!(::Sample, state, vn, dist, obs)
+    state.logp[] += logpdf(dist, obs)
+    return state
+end
+
+
+const coinflip = SimpleModel() do evaluator, y
+    state = init_state(evaluator)
+    θ, state = assume!(evaluator, state, @varname(θ), Beta(1, 1))
+
+    N = length(y)
+    for i = 1:N
+        state = observe!(evaluator, state, @varname(y[i]), Bernoulli(θ), y[i])
+    end
+
+    return state
+end
+
+model = coinflip([1,1,0])
+println(model(Sample()))
+
+
+
+
 
 end # module
