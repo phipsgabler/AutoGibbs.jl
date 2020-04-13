755364bf26535d96fc108f8ce1f681b668664772
Philipp Gabler <phipsgabler@users.noreply.github.com>
2020-04-13 13:57:14 +0200

    Useful stuff to test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,17 @@
 using AutoGibbs
 using Test
 
+using Distributions
+
+
 @testset "AutoGibbs.jl" begin
-    # Write your own tests here.
+    (α_0, θ_0) = (2.0, inv(3.0))
+    @model inverse_gdemo(x) = begin
+        λ ~ Gamma(α_0, θ_0)
+        m ~ Normal(0, sqrt(1 / λ))
+        x ~ Normal(m, sqrt(1 / λ))
+    end
+
+    model = inverse_gdemo(1.4)
+    st = AutoGibbs.strip_calls(trackmodel(model))
 end
