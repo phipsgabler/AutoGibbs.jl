cfc13384570cff59bc3f9005f01d041af6cfbfc2
Philipp Gabler <phipsgabler@users.noreply.github.com>
2020-04-17 16:26:27 +0200

    Dependency slicing works

--- src/dependencies.jl
+++ src/dependencies.jl
@@ -1,7 +1,7 @@
 using DynamicPPL
 using Distributions
 
-export dependencies
+export dependencies, makegraph, showgraph
 
 
 const CallingNode{F} = Union{PrimitiveCallNode{<:Any, F}, NestedCallNode{<:Any, F}}
@@ -17,25 +17,25 @@ istilde(::AbstractNode) = false
 function extract(node::CallingNode{typeof(DynamicPPL.tilde_assume)})
     args = node.call.arguments # ctx, sampler, right, vn, inds, vi
     vn, dist, (value, logp) = args[4], args[3], getvalue(node)
-    return vn, dist, value, logp
+    return vn, dist, TapeConstant(value), logp
 end
 
 function extract(node::CallingNode{typeof(DynamicPPL.tilde_observe)})
     args = node.call.arguments
     if length(args) == 7
         # ctx, sampler, right, left, vname, vinds, vi
-        vn, dist, value = args[5], args[3], args[4]
-        return vn, dist, value, nothing
+        vn, dist, value, logp = args[5], args[3], args[4], getvalue(node)
+        return vn, dist, value, logp
     elseif length(args) == 5
         # ctx, sampler, right, left, vi
-        dist, value = args[3], args[4]
-        return nothing, dist, value, nothing
+        dist, value, logp = args[3], args[4], getvalue(node)
+        return nothing, dist, value, logp
     end
 end
 
 function extract(node::CallingNode{typeof(DynamicPPL.dot_tilde_assume)})
     args = node.call.arguments # ctx, sampler, right, left, vn, inds, vi
-    vn, dist, (value, logp) = args[5], args[3], getvalue(node)
+    vn, dist, value, logp = args[5], args[3], args[4], getvalue(node)[2]
     return vn, dist, value, logp
 end
 
@@ -43,12 +43,12 @@ function extract(node::CallingNode{typeof(DynamicPPL.dot_tilde_observe)})
     args = node.call.arguments
     if length(args) == 7
         # ctx, sampler, right, left, vn, inds, vi
-        vn, dist, value = args[5], args[3], args[4]
-        return vn, dist, value, nothing
+        vn, dist, value, logp = args[5], args[3], args[4], getvalue(node)
+        return vn, dist, value, logp
     elseif length(args) == 5
         # ctx, sampler, right, left, vi
-        dist, value = args[3], args[4]
-        return nothing, dist, value, nothing
+        dist, value, logp = args[3], args[4], getvalue(node)
+        return nothing, dist, value, logp
     end
 end
 
@@ -86,3 +86,136 @@ function dependencies(node::NestedCallNode)
 
     return sort(deps; by=n -> getposition(n.info)) |> unique
 end
+
+
+
+
+
+
+struct Reference
+    name::Int
+end
+
+abstract type DepNode end
+
+struct Assumption{T} <: DepNode
+    vn::VarName
+    dist::Reference
+    value::T
+    logp::Float64
+end
+
+struct Observation{T} <: DepNode
+    vn::VarName
+    dist::Reference
+    value::T
+    logp::Float64
+end
+
+struct Call{T, TF, TArgs<:Tuple} <: DepNode
+    value::T
+    f::TF
+    args::TArgs
+end
+
+struct Constant{T} <: DepNode
+    value::T
+end
+
+
+function newname!(names, node)
+    newname = Reference(length(names) + 1)
+    names[getposition(node)] = newname
+    return newname
+end
+    
+
+function pushnode!(graph, names, node::CallingNode)
+    name = newname!(names, node)
+    f = convertvalue(names, node.call.f)
+    args = convertvalue.(Ref(names), (node.call.arguments..., something(node.call.varargs, ())...))
+    graph[name] = Call(getvalue(node), f, args)
+end
+
+function pushnode!(graph, names, node::CallingNode{<:Union{typeof(DynamicPPL.tilde_assume),
+                                                           typeof(DynamicPPL.dot_tilde_assume)}})
+    name = newname!(names, node)
+    vn_r, dist_r, value_r, logp = extract(node)
+    vn = getvalue(vn_r)
+    dist = convertvalue(names, dist_r)
+    value = getvalue(value_r)
+    graph[name] = Assumption(vn, dist, value, logp)
+end
+
+function pushnode!(graph, names, node::CallingNode{<:Union{typeof(DynamicPPL.tilde_observe),
+                                                           typeof(DynamicPPL.dot_tilde_observe)}})
+    name = newname!(names, node)
+    vn_r, dist_r, value_r, logp = extract(node)
+    vn = getvalue(vn_r)
+    dist = convertvalue(names, dist_r)
+    value = getvalue(value_r)
+    graph[name] = Observation(vn, dist, value, logp)
+end
+
+function pushnode!(graph, names, node::CallingNode{typeof(getindex)})
+    # special handling to get rid of the tuple result of `tilde_assume`
+    
+    array_ref = convertvalue(names, node.call.arguments[1])
+    if graph[array_ref] isa Assumption
+        names[getposition(node)] = array_ref
+    else
+        # fall back to default case
+        invoke(pushnode!, Tuple{typeof(graph), typeof(names), CallingNode}, graph, names, node)
+    end
+end
+
+function pushnode!(graph, names, node::ConstantNode)
+    name = newname!(names, node)
+    graph[name] = Constant(getvalue(node))
+end
+
+function pushnode!(graph, names, node::ArgumentNode)
+    if !isnothing(node.branch_node)
+        # skip branch argument nodes
+        original = getparent(node)
+        names[getposition(node)] = names[getposition(original)]
+    else
+        # function argument nodes are handled like constants
+        name = newname!(names, node)
+        graph[name] = Constant(getvalue(node))
+    end
+end
+
+
+convertvalue(names, value::TapeReference) = names[value.index]
+convertvalue(names, value::TapeConstant) = getvalue(value)
+
+function makegraph(slice::Vector{<:AbstractNode})
+    graph = Dict{Reference, DepNode}()  # resulting graph
+    names = Dict{Int, Reference}()      # mapping from node indices to new references
+    
+    for node in slice
+        pushnode!(graph, names, node)
+    end
+
+    return graph
+end
+
+
+Base.show(io::IO, r::Reference) = print(io, "%", r.name)
+Base.show(io::IO, expr::Assumption) = print(io, expr.vn, " ~ ", expr.dist, " = ", expr.value)
+Base.show(io::IO, expr::Observation) = print(io, expr.vn, " ~ ", expr.dist, " = ", expr.value)
+Base.show(io::IO, expr::Call) = print(io, expr.f, "(", join(expr.args, ", "), ") = ", expr.value)
+Base.show(io::IO, expr::Constant) = print(io, expr.value)
+
+
+function showgraph(graph)
+    for (r, c) in sort(graph; by = ref -> ref.name)
+        println(r, ": ", c)
+    end
+end
+
+
+
+
+
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,57 +2,88 @@ using AutoGibbs
 using Test
 
 using Distributions
+using DynamicPPL
 
+
+varnames(graph) = Set(tilde.vn
+                      for tilde in values(graph)
+                      if tilde isa Union{AutoGibbs.Assumption, AutoGibbs.Observation})
 
 @testset "AutoGibbs.jl" begin
-    @model function test0(x)
-        λ ~ Gamma(2.0, inv(3.0))
-        m ~ Normal(0, sqrt(1 / λ))
-        x ~ Normal(m, sqrt(1 / λ))
-    end
+    # @model function test0(x)
+    #     λ ~ Gamma(2.0, inv(3.0))
+    #     m ~ Normal(0, sqrt(1 / λ))
+    #     x ~ Normal(m, sqrt(1 / λ))
+    # end
 
-    t0 = trackmodel(test0(1.4)) |> AutoGibbs.strip_calls
-    d0 = dependencies(t0)
+    # t0 = trackmodel(test0(1.4)) |> AutoGibbs.strip_calls
+    # d0 = dependencies(t0)
+    # @test varnames(makegraph(d0)) == Set([@varname(λ), @varname(m), @varname(x)])
 
 
-    @model function test1(x)
-        s ~ Gamma(1.0, 1.0)
-        x .~ Normal(0.0, s)
-    end
+    # @model function test1(x)
+    #     s ~ Gamma(1.0, 1.0)
+    #     x .~ Normal(0.0, s)
+    # end
 
-    t1 = trackmodel(test1([-0.5, 0.5])) |> AutoGibbs.strip_calls
-    d1 = dependencies(t1)
+    # t1 = trackmodel(test1([-0.5, 0.5])) |> AutoGibbs.strip_calls
+    # d1 = dependencies(t1)
+    # @test varnames(makegraph(d1)) == Set([@varname(s), @varname(x)])
     
-    @model function test2(x)
-        s ~ Gamma(1.0, 1.0)
-        for i in eachindex(x)
-            x[i] ~ Normal(float(i), s)
-        end
-    end
-
-    t2 = trackmodel(test2([-0.5, 0.5])) |> AutoGibbs.strip_calls
-    d2 = dependencies(t2)
     
-    @model function test3(w)
-        p ~ Beta(1, 1)
-        x ~ Bernoulli(p)
-        y ~ Bernoulli(p)
-        z ~ Bernoulli(p)
-        w ~ MvNormal([x, y, z], 1.2)
-    end
+    # @model function test2(x)
+    #     s ~ Gamma(1.0, 1.0)
+    #     for i in eachindex(x)
+    #         x[i] ~ Normal(float(i), s)
+    #     end
+    # end
 
-    t3 = trackmodel(test3([1, 0, 1])) |> AutoGibbs.strip_calls
-    d3 = dependencies(t3)
+    # t2 = trackmodel(test2([-0.5, 0.5])) |> AutoGibbs.strip_calls
+    # d2 = dependencies(t2)
+    # @test varnames(makegraph(d2)) == Set([@varname(s), @varname(x[1]), @varname(x[2])])
+    
+    
+    # @model function test3(w)
+    #     p ~ Beta(1, 1)
+    #     x ~ Bernoulli(p)
+    #     y ~ Bernoulli(p)
+    #     z ~ Bernoulli(p)
+    #     w ~ MvNormal([x, y, z], 1.2)
+    # end
 
-    @model function test4(x) 
+    # t3 = trackmodel(test3([1, 0, 1])) |> AutoGibbs.strip_calls
+    # d3 = dependencies(t3)
+    # @test varnames(makegraph(d3)) == Set([@varname(p), @varname(x), @varname(y), @varname(y),
+    #                                       @varname(z), @varname(w)])
+    
+
+    # @model function test4(x) 
+    #     μ ~ MvNormal(fill(0, 2), 2.0)
+    #     z = Vector{Int}(undef, length(x))
+    #     for i in 1:length(x)
+    #         z[i] ~ Categorical([0.5, 0.5])
+    #         x[i] ~ Normal(μ[z[i]], 0.1)
+    #     end
+    # end
+
+    # t4 = trackmodel(test4([1, 1, -1])) |> AutoGibbs.strip_calls
+    # d4 = dependencies(t4)
+    # @test varnames(makegraph(d4)) == Set([@varname(μ),
+    #                                       @varname(z[1]), @varname(z[2]), @varname(z[3]),
+    #                                       @varname(x[1]), @varname(x[2]), @varname(x[3])])
+
+    
+    @model function test5(x) 
         μ ~ MvNormal(fill(0, 2), 2.0)
         z = Vector{Int}(undef, length(x))
+        z .~ Categorical.(fill([0.5, 0.5], length(x)))
         for i in 1:length(x)
-            z[i] ~ Categorical([0.5, 0.5])
             x[i] ~ Normal(μ[z[i]], 0.1)
         end
     end
 
-    t4 = trackmodel(test4([1, 1, -1])) |> AutoGibbs.strip_calls
-    d4 = dependencies(t4)
+    t5 = trackmodel(test5([1, 1, -1])) |> AutoGibbs.strip_calls
+    d5 = dependencies(t5)
+    @test varnames(makegraph(d5)) == Set([@varname(μ), @varname(z),
+                                          @varname(x[1]), @varname(x[2]), @varname(x[3])])
 end
