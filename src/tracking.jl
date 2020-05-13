b0883ff5424bb8636bf46ea9852e09c9221ffcf1
Philipp Gabler <phipsgabler@users.noreply.github.com>
2020-05-13 15:13:28 +0200

    Update tracking code to newer DPPL version

--- src/tracking.jl
+++ src/tracking.jl
@@ -4,20 +4,37 @@ using DynamicPPL
 export trackmodel, strip_model_layers
 
 
+const ModelEval = Union{typeof(DynamicPPL.evaluate_singlethreaded),
+                        typeof(DynamicPPL.evaluate_multithreaded)}
+
+
 struct AutoGibbsContext{F} <: AbstractTrackingContext end
 
-IRTracker.canrecur(ctx::AutoGibbsContext, ::Model, args...) = true
+IRTracker.canrecur(ctx::AutoGibbsContext{F}, ::Model{F}, args...) where {F} = true
+IRTracker.canrecur(ctx::AutoGibbsContext{F}, ::ModelEval, ::Model{F}, args...) where {F} = true
 IRTracker.canrecur(ctx::AutoGibbsContext{F}, f::F, args...) where {F} = true
 IRTracker.canrecur(ctx::AutoGibbsContext, f, args...) = false
+
 
 trackmodel(model::Model{F}) where {F} = track(AutoGibbsContext{F}(), model)
 
 
-function strip_model_layers(node::NestedCallNode{<:Any, F, <:Tuple{<:Model{F}, Vararg}}) where F
+ismodeleval(::NestedCallNode{<:Any, F, <:Tuple{<:Model{F}, Vararg}}) where {F} = true
+ismodeleval(::NestedCallNode{<:Any, <:ModelEval, <:Tuple{<:Model, Vararg}}) = true
+ismodeleval(::NestedCallNode{<:Any, <:Model}) = true
+ismodeleval(::AbstractNode) = false
+
+function strip_model_layers(node::NestedCallNode{<:Any, F, <:Tuple{<:Model{F}, Vararg}}) where {F}
+    # base case: call of the model closure, F
     return node
 end
 
-function strip_model_layers(node::NestedCallNode{<:Any, <:Model{F}}) where {F}
-    ix = findfirst(c -> c isa NestedCallNode{<:Any, <:Union{<:Model{F}, F}}, getchildren(node))
-    return strip_model_layers(getchildren(node)[ix])
+function strip_model_layers(node::NestedCallNode)
+    if ismodeleval(node)
+        children = getchildren(node)
+        ix = findfirst(ismodeleval, children)
+        return strip_model_layers(children[ix])
+    else
+        throw(ArgumentError("$node is not a model call, you should not have reached this point!"))
+    end
 end
