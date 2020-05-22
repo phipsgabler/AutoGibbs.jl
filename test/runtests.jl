d5836961585bc8a2a1dc0e7413a00637de7982f4
Philipp Gabler <phipsgabler@users.noreply.github.com>
2020-05-22 14:49:23 +0200

    Add test cases for complete dependencies

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,20 @@
-using AutoGibbs
 using Test
 
+using AutoGibbs
 using Distributions
 using DynamicPPL
-# using Turing, Turing.RandomMeasures
+using Turing, Turing.RandomMeasures
 
 
 function varnames(graph)
     vars = (ref for ref in keys(graph) if ref isa AutoGibbs.NamedReference)
     return Set(AutoGibbs.resolve_varname(graph, ref) for ref in vars)
 end
+
+check_dependencies(graph) = all(haskey(graph, dep)
+                                for expr in values(graph)
+                                for dep in AutoGibbs.dependencies(expr))
+
 
 @testset "AutoGibbs.jl" begin
     @model function test0(x)
@@ -20,6 +25,7 @@ end
 
     graph0 = trackdependencies(test0(1.4))
     @test varnames(graph0) == Set([@varname(λ), @varname(m), @varname(x)])
+    @test check_dependencies(graph0)
 
 
     @model function test1(x)
@@ -29,7 +35,8 @@ end
 
     graph1 = trackdependencies(test1([-0.5, 0.5]))
     @test varnames(graph1) == Set([@varname(s), @varname(x)])
-    
+    @test check_dependencies(graph1)
+
     
     @model function test2(x)
         s ~ Gamma(1.0, 1.0)
@@ -40,6 +47,7 @@ end
 
     graph2 = trackdependencies(test2([-0.5, 0.5]))
     @test varnames(graph2) == Set([@varname(s), @varname(x[1]), @varname(x[2])])
+    @test check_dependencies(graph2)
     
     
     @model function test3(w)
@@ -53,7 +61,8 @@ end
     graph3 = trackdependencies(test3([1, 0, 1]))
     @test varnames(graph3) == Set([@varname(p), @varname(x), @varname(y), @varname(y),
                                    @varname(z), @varname(w)])
-    
+    @test check_dependencies(graph3)
+
     
     @model function test4(x) 
         μ ~ MvNormal(fill(0, 2), 2.0)
@@ -68,7 +77,8 @@ end
     @test varnames(graph4) == Set([@varname(μ),
                                    @varname(z[1]), @varname(z[2]), @varname(z[3]),
                                    @varname(x[1]), @varname(x[2]), @varname(x[3])])
-    
+    @test check_dependencies(graph4)
+
     
     @model function test5(x) 
         μ ~ MvNormal(fill(0, 2), 2.0)
@@ -82,6 +92,7 @@ end
     graph5 = trackdependencies(test5([1, 1, -1]))
     @test varnames(graph5) == Set([@varname(μ), @varname(z),
                                    @varname(x[1]), @varname(x[2]), @varname(x[3])])
+    @test check_dependencies(graph5)
 
 
     @model function test6(x, y)
@@ -94,6 +105,7 @@ end
 
     graph6 = trackdependencies(test6(1.4, 1.2))
     @test varnames(graph6) == Set([@varname(λ), @varname(m), @varname(x), @varname(y)])
+    @test check_dependencies(graph6)
 
     
     @model function test7(x)
@@ -106,7 +118,8 @@ end
 
     graph7 = trackdependencies(test7([0.0, 0.1, -0.2]))
     @test varnames(graph7) == Set([@varname(s), @varname(x[1]), @varname(x[2]), @varname(x[3])])
-    
+    @test check_dependencies(graph7)
+
     
     @model function test8()
         s ~ Gamma(1.0, 1.0)
@@ -115,6 +128,7 @@ end
 
     graph8 = trackdependencies(test8())
     @test varnames(graph8) == Set([@varname(s)])
+    @test check_dependencies(graph8)
 
     
     @model function test9(x)
@@ -132,7 +146,8 @@ end
                                    @varname(x[1]), @varname(x[2]), @varname(x[3]),
                                    @varname(state[1]), @varname(state[2]), @varname(state[3]),
                                    @varname(state[4])])
-    
+    @test check_dependencies(graph9)
+
 
     @model function test10(x)
         s ~ Gamma(1.0, 1.0)
@@ -148,18 +163,25 @@ end
     @test varnames(graph10) == Set([@varname(s),
                                    @varname(x[1]), @varname(x[2]), @varname(x[3]),
                                    @varname(state[2]), @varname(state[3]), @varname(state[4])])
+    @test check_dependencies(graph10)
 
 
-    # @model function dpmixture(x)
-    #     rpm = DirichletProcess(1.0)
-    #     n = zeros(Int, length(x))
-    #     z = zeros(Int, length(x))
-    #     for i in eachindex(x)
-    #         z[i] ~ ChineseRestaurantProcess(rpm, n)
-    #         n[z[i]] += 1
-    #     end
-    #     K = findlast(!iszero, n)
-    #     m ~ MvNormal(fill(0.0, K), 1.0)
-    #     x ~ MvNormal(m[z], 1.0)
-    # end
+    @model function test11(x)
+        rpm = DirichletProcess(1.0)
+        n = zeros(Int, length(x))
+        z = zeros(Int, length(x))
+        for i in eachindex(x)
+            z[i] ~ ChineseRestaurantProcess(rpm, n)
+            n[z[i]] += 1
+        end
+        K = findlast(!iszero, n)
+        m ~ MvNormal(fill(0.0, K), 1.0)
+        x ~ MvNormal(m[z], 1.0)
+    end
+
+    graph11 = trackdependencies(test11([0.1, 0.05, 1.0]))
+    @test varnames(graph11) == Set([@varname(m), @varname(x),
+                                    @varname(z[1]), @varname(z[2]), @varname(z[3])])
+    @test check_dependencies(graph11)
+
 end
