45da54c2ae3650ba5d3e5c3d7cbb094255c47eae
Philipp Gabler <phipsgabler@users.noreply.github.com>
2020-09-28 20:53:17 +0200

    Write stick-breaking model

--- test/test_imms.jl
+++ test/test_imms.jl
@@ -17,6 +17,86 @@ DP(α, G₀) = DirichletProcess(α)
 
 
 ###########################################################################
+
+function stickbreak(v)
+    K = length(v) + 1
+    cumprod_one_minus_v = cumprod(1 .- v)
+
+    return map(1:K) do k
+        if k == 1
+            v[1]
+        elseif k == K
+            cumprod_one_minus_v[K - 1]
+        else
+            v[k] * cumprod_one_minus_v[k - 1]
+        end
+    end
+end
+
+# from https://luiarthur.github.io/TuringBnpBenchmarks/dpsbgmm
+@model function imm_stick(y, α, K)
+    N = length(y)
+
+    μ ~ filldist(Normal(0), K)
+
+    crm = DirichletProcess(α)
+    v ~ filldist(StickBreakingProcess(crm), K - 1)
+    w = stickbreak(v)
+
+    z = Vector{Int}(undef, N)
+    for n = 1:N
+        z[n] ~ Categorical(w)
+        y[n] ~ Normal(μ[z[n]], 1.0)
+    end
+end
+
+function test_imm_stick()
+    model_imm_stick = imm_stick(data_neal, α_neal, 10)
+    graph_imm_stick = trackdependencies(model_imm_stick)
+    
+    # we leave out the μs, because there might be 1--3 of them
+    @testdependencies(model_imm_stick,
+                      v[1], v[2], v[3], v[4], v[5], v[6], v[7], v[8], v[9], # 10 - 1 sticks
+                      μ[1], μ[2], μ[3], μ[4], μ[5], μ[6], μ[7], μ[8], μ[9], μ[10], # 10 cluster centers
+                      z[1], z[2], z[3], z[4], z[5], z[6], z[7], z[8], z[9], # 9 data points
+                      y[1], y[2], y[3], y[4], y[5], y[6], y[7], y[8], y[9])
+    cond_imm_stick = StaticConditional(model_imm_stick, :z)
+    @test_nothrow sample(model_imm_stick, Gibbs(cond_imm_stick, MH(:μ, :v)), 500)
+    @test_nothrow sample(model_imm_stick, Gibbs(cond_imm_stick, HMC(0.01, 10, :μ, :v)), 500)
+
+    # Analytic tests
+    μ = graph_imm_stick[9].value
+    v = graph_imm_stick[14].value
+    z = [v.value for v in values(graph_imm_stick.statements)
+         if v isa AutoGibbs.Assumption && DynamicPPL.subsumes(@varname(z), v.vn)]
+    y = graph_imm_stick[2].value
+    K = graph_imm_stick[6].value
+    N = graph_imm_stick[7].value
+    α = graph_imm_stick[4].value
+
+    D_w = Categorical(stickbreak(v))
+    p_z = [logpdf(D_w, z) + logpdf(Normal(μ[z]), y[n]) for z = 1:K, n = 1:N]
+    analytic_conditionals = map(1:N) do n
+        p̃ = [logpdf(D_w, z) + logpdf(Normal(μ[z]), y[n]) for z = 1:K]
+        @varname(z[n]) => Categorical(p̃ ./ sum(p̃))
+    end
+    @info "stick-breaking IMM analytic conditionals" analytic_conditionals
+    
+    θ = AutoGibbs.sampled_values(graph_imm_stick)
+    
+    local calculated_conditionals
+    @test_nothrow calculated_conditionals = conditionals(graph_imm_stick, @varname(z))
+    @info "stick-breaking IMM calculated conditionals" Dict(
+        vn => cond(θ) for (vn, cond) in calculated_conditionals)
+    
+    for (vn, analytic_conditional) in analytic_conditionals
+        # @show vn => probs(calculated_conditionals[vn]), probs(analytic_conditional)
+        @test issimilar(calculated_conditionals[vn](θ), analytic_conditional)
+    end
+end
+
+
+###########################################################################
 # version as this would usually be written in Turing, with the slight
 # modification that we need to associate G₀ with z (to calculate marginals of
 # y for "unobserved clusters")
@@ -287,6 +367,7 @@ end
 ##########################################################################
 #########################################################################
 ### TEST TOGGLES
-test_imm()
+test_imm_stick()
+# test_imm()
 # test_imm_oneshot()
 # test_imm_manual()
