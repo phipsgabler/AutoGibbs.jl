d0f819a7c7446280659206f4bf254b307e2a9cd5
Philipp Gabler <phipsgabler@users.noreply.github.com>
2020-10-05 15:02:43 +0200

    Merge PG model code

--- test/models_comparison.jl
+++ test/models_comparison.jl
@@ -9,12 +9,10 @@
 
     # Cluster assignments.
     z = tzeros(Int, N)
-    for n = 1:N
-        z[n] ~ DiscreteNonParametric(1:K, w)
-    end
-    
+
     # Observations.
     for n = 1:N
+        z[n] ~ DiscreteNonParametric(1:K, w)
         x[n] ~ Normal(μ[z[n]], 1.0)
     end
 
@@ -32,32 +30,24 @@ gmm_generate(N; K = 2) = identity.(gmm_tarray(fill(missing, N), K)())
     s = tzeros(Int, N)
 
     # Emission matrix.
-    m = Vector{T}(undef, K)
+    m ~ arraydist([Normal(i, 0.5) for i in 1:K])
 
     # Transition matrix.
-    T = Vector{Vector{T}}(undef, K)
+    t ~ filldist(Dirichlet(K, 1.0), K)
 
-    # Assign distributions to each element
-    # of the transition matrix and the
-    # emission matrix.
-    for i = 1:K
-        T[i] ~ Dirichlet(K, 1.0)
-        m[i] ~ Normal(i, 0.5)
-    end
-    
     # Observe each point of the input.
     s[1] ~ Categorical(K)
     x[1] ~ Normal(m[s[1]], 0.1)
 
     for i = 2:N
-        s[i] ~ Categorical(T[s[i-1]])
+        s[i] ~ Categorical(t[:,s[i-1]])
         x[i] ~ Normal(m[s[i]], 0.1)
     end
 
     return x
 end
 
-hmm_tarray_example(x = [0.1, -0.05, 1.0], K = 2) = hmm_tarray(x, K)
+hmm_tarray_example(;x = [0.1, -0.05, 1.0], K = 2) = hmm_tarray(x, K)
 hmm_generate(N; K = 2) = identity.(hmm_tarray(fill(missing, N), K)())
 
 function stickbreak(v)
