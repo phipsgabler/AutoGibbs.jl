5b5556821f60d66cbd4f01b0db952b84b58b7265
Philipp Gabler <phipsgabler@users.noreply.github.com>
2020-10-10 16:48:05 +0200

    Completely rewrite experiments code and permute all conditions

--- test/experiments.jl
+++ test/experiments.jl
@@ -1,100 +1,109 @@
 using Turing
 using Turing.RandomMeasures
+using DynamicPPL
 using Random
 using AutoGibbs
-using BenchmarkTools
 using Plots
 using StatsPlots
 
 
 include("models.jl")
-include("models_comparison.jl")
 
-const plots_dir = "plots"
-
-const iter = 5_000    # sampling steps
-const N = 20          # size of test data set
-const particles = 10  # parameter for particle gibbs
-const lf_size = 0.1   # parameter 1 for HMC
-const n_step = 10     # parameter 2 for HMC
+const CHAIN_LENGTH = 10 #5_000    # sampling steps
+const HMC_LF_SIZE = 0.1   # parameter 1 for HMC
+const HMC_N_STEP = 10     # parameter 2 for HMC
+const BENCHMARK_CHAINS = 10 # number of chains to sample per combination
+const DATA_RNG = MersenneTwister(424242)
 
 
-function benchmark_models(combinations...; N=iter, bparams...)
+function run_experiments(
+    modelname,
+    generate,
+    example,
+    tarray_example,
+    p_discrete,
+    p_continuous
+)
+    results = []
+    compilation_times = []
+
     old_prog = Turing.PROGRESS[]
     Turing.turnprogress(false)
-    suite = BenchmarkGroup()
     
-    benchmarks = map(combinations) do (name, (m, alg))
-        suite[name] = @benchmarkable sample($m, $alg, $N)
+    for L in (10, 25, 50)
+        data = generate(DATA_RNG, L)
+        model_ag = example(x = data)
+        model_pg = tarray_example(x = data)
+
+        for particles in (5, 10, 15)
+            # get a new conditional for each particle size, so that we have
+            # a couple of samples of the compilation time for each L
+            start_time = time_ns()
+            static_conditional = StaticConditional(model_ag, p_discrete)
+            compilation_time = time_ns() - start_time
+            
+            push!(compilation_times,
+                  (model = modelname,
+                   data_size = L,
+                   compilation_time = compilation_time))
+            
+            mh = MH(p_continuous...)
+            hmc = HMC(HMC_LF_SIZE, HMC_N_STEP, p_continuous...)
+            pg = PG(particles, p_discrete)
+
+            combinations = Dict([
+                ("AG", "MH") => (model_ag, Gibbs(static_conditional, mh)),
+                ("AG", "HMC") => (model_ag, Gibbs(static_conditional, hmc)),
+                ("PG", "MH") => (model_pg, Gibbs(pg, mh)),
+                ("PG", "HMC") => (model_pg, Gibbs(pg, hmc))
+            ])
+
+            for ((d_alg, c_alg), (model, sampler)) in combinations
+                @info "Running $d_alg+$c_alg with data size $L and $particles particles"
+                for n in 1:BENCHMARK_CHAINS
+                    start_time = time_ns()
+                    chain = sample(model, sampler, CHAIN_LENGTH)
+                    # sample(model, sampler, MCMCThreads(), 10, N)
+                    sample_time = time_ns() - start_time
+                    
+                    @info "Chain $n ran in $(sample_time / 1e9)s"
+
+                    push!(results,
+                          (model = modelname,
+                           discrete_algorithm = d_alg,
+                           continuous_algorithm = c_alg,
+                           particles = particles,
+                           data_size = L,
+                           chain = chain))
+                end
+            end
+        end
     end
-    
-    tune!(suite)
-    results = run(suite; bparams...)
-    
+
     Turing.turnprogress(old_prog)
-    return results
+
+
+    return compare_samplers(m1, m2, p_discrete, p_continuous)
 end
 
+const MODEL_SETUPS = Dict([
+    "GMM" => (gmm_generate, gmm_example, gmm_tarray_example, :z, (:w, :μ)),
+    "HMM" => (hmm_generate, hmm_example, hmm_tarray_example, :s, (:t, :m)),
+    "IMM" => (imm_stick_generate, imm_stick_example, imm_stick_tarray_example, :z, (:μ, :v))
+])
 
-function compare_samplers(model_ag, model_pg, p_discrete, p_continuous)
-    static_conditional = StaticConditional(model_ag, p_discrete...)
-    alg1 = Gibbs(static_conditional, MH(p_continuous...))
-    alg2 = Gibbs(static_conditional, HMC(lf_size, n_step, p_continuous...))
-    alg3 = Gibbs(PG(particles, p_discrete...), MH(p_continuous...))
-    alg4 = Gibbs(PG(particles, p_discrete...), HMC(lf_size, n_step, p_continuous...))
-
-    combinations = Dict([
-        "AG+MH" => (model_ag, alg1),
-        "AG+HMC" => (model_ag, alg2),
-        "PG+MH" => (model_pg, alg3),
-        "PG+HMC" => (model_pg, alg4)])
+function main(modelname, results_path=nothing)
+    modelname = uppercase(modelname)
     
-    # b = benchmark_models(combinations...;
-                         # N=iter, verbose=true, samples=10)
-
-    pd = mkpath(plots_dir)
-    for (name, (m, alg)) in combinations
-        chain = sample(m, alg, iter)
-        # plot only the first of the cluster paramers, and all continuous ones
-        plt = plot(chain[[Symbol.(p_discrete, Ref("[1]"))..., p_continuous...]],
-                   title=name)
-        Plots.pdf(plt, joinpath(pd, name * ".pdf"))
+    if isnothing(results_path)
+        results_path = "."
     end
-    
-    # return b
+
+    if haskey(MODEL_SETUPS, modelname)
+        run_experiments(modelname, MODEL_SETUPS[modelname]...)
+    else
+        println("Unknown model: $modelname")
+    end
 end
 
-function compare_gmm()
-    data = gmm_generate(20)
-    m1 = gmm_example(x = data)
-    m2 = gmm_tarray_example(x = data)
-    p_discrete = [:z]
-    p_continuous = [:w, :μ]
-
-    return compare_samplers(m1, m2, p_discrete, p_continuous)
-end
-
-function compare_hmm()
-    data = hmm_generate(20)
-    m1 = hmm_example(x = data)
-    m2 = hmm_tarray_example(x = data)
-    p_discrete = [:s]
-    p_continuous = [:t, :m]
-
-    return compare_samplers(m1, m2, p_discrete, p_continuous)
-end
-
-function compare_imm()
-    m1 = imm_stick_example()
-    m2 = imm_stick_tarray_example()
-    p_discrete = [:z]
-    p_continuous = [:μ, :v]
-
-    return compare_samplers(m1, m2, p_discrete, p_continuous)
-end
-
-
-
-compare_gmm()
-# compare_hmm()
-# compaare_imm()
+main(ARGS...)
