6140a133ffe63f10cedf603cc620eac1a5dcd503
Philipp Gabler <phipsgabler@users.noreply.github.com>
2020-11-16 16:13:40 +0100

    Update readme and graphviz export

--- README.md
+++ README.md
@@ -3,10 +3,13 @@
 [![Build Status](https://travis-ci.com/phipsgabler/AutoGibbs.jl.svg?branch=master)](https://travis-ci.com/phipsgabler/AutoGibbs.jl)
 
 Beware: this is nothing but a proof of concept.  Specifically, due to unfortate choices in
-`IRTracker`, handling traces of models with data sets not particularly small leads to exploding
-type inference time, making the method unusable.
+`IRTracker`, handling traces of models with data sets not particularly small can lead to exploding
+type inference time, making the method unusable in some cases.
 
-
+There are some limitations with respect to kinds of observations supported: 
+- Only “simple” indexing is allowed
+- Slices may work, everything else is at your own risk
+- Broadcasted sampling is not working
 
 ## Dependency extraction in DPPL models
 
@@ -27,18 +30,21 @@ will result in
 
 ```
 ⟨2⟩ = 1.4
-⟨4⟩ = λ ~ Gamma(2.0, 0.3333333333333333) → 0.5181638066817373
-⟨5⟩ = /(1, ⟨4⟩) → 1.9298916425751296
-⟨6⟩ = sqrt(⟨5⟩) → 1.3892053997070157
-⟨8⟩ = m ~ Normal(0, ⟨6⟩) → 0.7621682353026793
-⟨9⟩ = /(1, ⟨4⟩) → 1.9298916425751296
-⟨10⟩ = sqrt(⟨9⟩) → 1.3892053997070157
-⟨12⟩ = x ⩪ Normal(⟨8⟩, ⟨10⟩) ← ⟨2⟩
+⟨3⟩ = Gamma(2.0, 0.3333333333333333) → Gamma{Float64}(α=2.0, θ=0.3333333333333333)
+⟨4⟩ = λ ~ ⟨3⟩ → 0.9881137307157533
+⟨5⟩ = /(1, ⟨4⟩) → 1.0120292522153667
+⟨6⟩ = sqrt(⟨5⟩) → 1.0059966462247112
+⟨7⟩ = Normal(0, ⟨6⟩) → Normal{Float64}(μ=0.0, σ=1.0059966462247112)
+⟨8⟩ = m ~ ⟨7⟩ → 1.0814646155873082
+⟨9⟩ = /(1, ⟨4⟩) → 1.0120292522153667
+⟨10⟩ = sqrt(⟨9⟩) → 1.0059966462247112
+⟨11⟩ = Normal(⟨8⟩, ⟨10⟩) → Normal{Float64}(μ=1.0814646155873082, σ=1.0059966462247112)
+⟨12⟩ = x ⩪ ⟨11⟩ ← ⟨2⟩
 ```
 
-Notation: references to values are written in ⟨angle brackets⟩.  The names of random variables in
-tilde statements are annotated with names (and possibly indices).  Unobserved random variables
-(“assumptions”) are notated by a simple `~`, observed ones use `⩪`.  Equality signs denote
+Notation: references to intermediate values are written in ⟨angle brackets⟩.  The names of random
+variables in tilde statements are annotated with names (and possibly indices).  Unobserved random
+variables (“assumptions”) are notated by a simple `~`, observed ones use `⩪`.  Equality signs denote
 assignment of intermediate deterministic values, as in normal SSA-form IR.  The results of
 deterministic or assumed values come after the `→`; `←` is used for values that are observed.
 
@@ -51,18 +57,21 @@ The result of `trackdependencies` is a `Graph`, which is essentially an ordered 
 
 ```
 julia> graph0[4]
-Gamma(2.0, 0.3333333333333333) → 1.0351608689025245
+λ ~ ⟨3⟩ → 0.9881137307157533
 ```
 
 You can also write the graph to a GraphViz dot file:
 
 ```
 julia> AutoGibbs.savedot("/tmp/graph.dot", graph0)
-
-shell> dot /tmp/graph.dot -Tpdf > /tmp/graph.pdf
 ```
 
-But don’t expect any magic to make it look good:
+and covert it to PDF with
+
+```
+# Noto fonts are only required for fancy unicode, but otherwise some things will look ugly.
+dot /tmp/graph.dot -Tpdf -Nfontname="Noto Mono" -Efontname="Noto Mono" > /tmp/graph.pdf
+```
 
 ![Dependency graph output](./images/graph.png)
 
--- src/plotting.jl
+++ src/plotting.jl
@@ -1,42 +1,59 @@
 using IRTracker.GraphViz
 
+escape_show(x) = escape_string(sprint(show, x))
 
 function pushstmt!(stmts, ref, stmt::Constant)
-    value = escape_string(sprint(showstmt, ref, stmt))
+    value = escape_show(ref) * " = " * escape_show(stmt.value)
     push!(stmts, GraphViz.Node(string(ref.number), label=value, shape="rectangle"))
 end
 function pushstmt!(stmts, ref, stmt::Call)
-    argstring = join(stmt.args, ", ")
-    label = escape_string(sprint(showstmt, ref, stmt))
+    argstring = join((escape_show(arg) for arg in stmt.args), ", ")
+    label = escape_show(ref) * " = " * escape_show(stmt.f) * "(" * argstring * ")"
     push!(stmts, GraphViz.Node(string(ref.number), label=label, shape="rectangle"))
 end
-function pushstmt!(stmts, ref, stmt::Union{Assumption, Observation})
-    label = escape_string(sprint(showstmt, ref, stmt))
+function pushstmt!(stmts, ref, stmt::Assumption)
+    label = escape_show(ref) * " ~ " * escape_show(stmt.dist_ref)
+    push!(stmts, GraphViz.Node(string(ref.number), label=label, shape="circle"))
+end
+function pushstmt!(stmts, ref, stmt::Observation)
+    label = escape_show(ref) * " ~̇ " * escape_show(stmt.dist_ref) * " ← " * escape_show(stmt.value)
     push!(stmts, GraphViz.Node(string(ref.number), label=label, shape="circle"))
 end
 
 
 function Base.convert(::Type{GraphViz.Graph}, graph::Graph)
     stmts = Vector{GraphViz.Statement}()
+    nodes = Dict{Statement, GraphViz.NodeID}()
 
     for (ref, stmt) in graph
+        # node for stmt itself
         pushstmt!(stmts, ref, stmt)
-        
-        for dep in dependencies(stmt)
-            from = GraphViz.NodeID(string(ref.number))
-            to = GraphViz.NodeID(string(dep.number))
-            push!(stmts, GraphViz.Edge([from, to]))
-        end
+        from = GraphViz.NodeID(string(ref.number))
+        nodes[stmt] = from
 
-        if ref isa NamedReference
-            from = GraphViz.NodeID(string(ref.number))
-            @show ref
-            for index in DynamicPPL.getindexing(ref.vn), ix in index
-                if ix isa Reference
-                    to = GraphViz.NodeID(string(ix.number))
-                    @show ix
+        if stmt isa Tilde
+            # tilde to distribution
+            dist = nodes[graph[stmt.dist_ref]]
+            push!(stmts, GraphViz.Edge([from, dist]))
+
+            # tilde to observed value
+            if stmt isa Observation && stmt.value isa Reference
+                to = nodes[graph[stmt.value]]
+                push!(stmts, GraphViz.Edge([from, to]))
+            end
+        elseif stmt isa Call
+            # call to each parent argument
+            for arg in stmt.args
+                if arg isa Reference
+                    to = nodes[graph[arg]]
                     push!(stmts, GraphViz.Edge([from, to]))
                 end
+            end
+
+            # call to non-constant function
+            if stmt.f isa Reference
+                to = nodes[graph[arg]]
+                push!(stmts, GraphViz.Edge([from, to]))
             end
         end
     end
@@ -61,5 +78,5 @@ function savedot(fn::AbstractString, graph::Graph)
 end
 
 
-# dot /tmp/graph.dot -Tpdf -Nfontname="DejaVu Sans Mono" -Efontname="DejaVu Sans Mono" > /tmp/graph.pdf
+# dot /tmp/graph.dot -Tpdf -Nfontname="Noto Mono" -Efontname="Noto Mono" > /tmp/graph.pdf
 
