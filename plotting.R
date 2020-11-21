210014e8862178134479be35be1eb0ea8de331a2
Philipp Gabler <phipsgabler@users.noreply.github.com>
2020-11-21 18:46:17 +0100

    Parametrize plotting code

--- plotting.R
+++ plotting.R
@@ -3,11 +3,16 @@ library(tidyverse)
 base_font <- "Linux Biolinum O"
 theme_set(theme_light(base_family = base_font))
 
-sampling_times <- read_csv("results/GMM-sampling_times-2020-10-12T19:21:00.csv")
-diagnostics <- read_csv("results/GMM-diagnostics-2020-10-12T19:21:00.csv") %>%
+model <- "HMM"
+timestamp <- "2020-11-18T19:02:00"
+results_dir <- "results/"
+
+
+sampling_times <- read_csv(paste0(results_dir, model, "-sampling_times-", timestamp, ".csv"))
+diagnostics <- read_csv(paste0(results_dir, model, "-diagnostics-", timestamp, ".csv")) %>%
     mutate(diagnostic = as.factor(diagnostic))
-chains <- read_csv("results/GMM-chains-2020-10-12T19:21:00.csv")
-compile_times <- read_csv("results/GMM-compile_times-2020-10-12T19:21:00.csv") %>%
+chains <- read_csv(paste0(results_dir, model, "-chains-", timestamp, ".csv"))
+compile_times <- read_csv(paste0(results_dir, model, "-compile_times-", timestamp, ".csv")) %>%
     mutate(is_first = repetition == 1)
 
 obs_labeller <- as_labeller(function(obs) paste(obs, "observations"))
@@ -16,7 +21,7 @@ isfirst_labeller <- as_labeller(function (p) ifelse(p, "First", "Other"))
 ## diagnostics_labeller <- function (d) ifelse(d == "ess", bquote(ESS), bquote(R))
 
 
-samplingtime_plot_gmm <-
+samplingtime_plot <-
     sampling_times %>%
     arrange(particles) %>%
     mutate(algorithm = as_factor(str_c(discrete_algorithm, " & ", continuous_algorithm,
@@ -32,13 +37,13 @@ samplingtime_plot_gmm <-
     facet_grid(. ~ algorithm, scales = "free_x") +
     scale_x_continuous(breaks = unique(sampling_times$data_size)) +
     labs(x = "Observations (data size)", y = "Sampling time (s)",
-         color = "Observations", title = "Sampling times",
-         subtitle = "Factored by algorithm and number of PG particles")
-ggsave("results/GMM-sampling_times.pdf", samplingtime_plot_gmm, device = cairo_pdf)
+         color = "Observations", title = paste("Sampling times for", model))
+ggsave(paste0(results_dir, model, "-sampling_times.pdf"), samplingtime_plot, device = cairo_pdf)
 
-diagnostics_plot_gmm <- diagnostics %>%
-    ## filter(diagnostic == "ess", data_size != 25) %>%
-    filter(!startsWith(parameter, "z") | parameter == "z[1]") %>%
+diagnostics_plot <-
+    diagnostics %>%
+    filter(diagnostic == "ess") %>%
+    filter(parameter == "s[5]" | parameter == "m[1]") %>%
     arrange(particles) %>%
     mutate(algorithm = as_factor(str_c(discrete_algorithm, " & ", continuous_algorithm,
                                        ifelse(discrete_algorithm == "PG",
@@ -46,25 +51,27 @@ diagnostics_plot_gmm <- diagnostics %>%
                                               "")))) %>%
     ggplot(aes(x = data_size,
                ## group = interaction(data_size, parameter),
-               group = data_size,
+               color = as_factor(data_size),
                y = value)) +
     ## geom_boxplot(outlier.alpha = 0.2, position = position_dodge2()) +
     ## geom_boxplot(outlier.alpha = 0.2) + 
-    geom_jitter(alpha = 0.5) + 
+    geom_jitter(alpha = 0.5, height = 0) + 
     facet_grid(diagnostic + parameter ~ algorithm, scales = "free_x") +
     scale_x_continuous(breaks = unique(diagnostics$data_size)) +
     labs(x = "Observations (data size)", y = "Value of diagnostic",
-         color = "Observations", title = "Convergence diagnostics",
-         subtitle = "Factored by algorithm and number of particles")
-ggsave("results/GMM-diagnostics.pdf", diagnostics_plot_gmm, device = cairo_pdf)
+         color = "Observations", title = paste("Convergence diagnostics for", model))
+ggsave(paste0(results_dir, model, "-diagnostics.pdf"), diagnostics_plot, device = cairo_pdf)
 
 
-thin_chains <- chains %>%
+thin_chains <-
+    chains %>%
     filter(discrete_algorithm == "AG", repetition %in% 1:3, step %% 37 == 0, particles == 10) %>%
-    filter((!startsWith(parameter, "z") | parameter == "z[1]")) %>%
+    filter(parameter == "s[5]" | parameter == "m[2]") %>%
     select(-model, -discrete_algorithm, -continuous_algorithm, -particles)
 
-densities_plot_gmm <- thin_chains %>%
+densities_plot <-
+    thin_chains %>%
+    filter(parameter == "s[5]") %>% 
     ## arrange(particles) %>%
     ## mutate(algorithm = as_factor(str_c(discrete_algorithm, " & ", continuous_algorithm,
                                        ## ifelse(discrete_algorithm == "PG",
@@ -78,13 +85,12 @@ densities_plot_gmm <- thin_chains %>%
                scales = "free") +
     guides(color = F) + 
     labs(x = "Value", y = "Probability",
-         title = "AG posterior densities",
-         subtitle = paste("Factored by number of observations (data size)",
-                          "and selected parameters"))
-ggsave("results/GMM-densities.pdf", densities_plot_gmm, device = cairo_pdf)
+         title = paste("AG posterior densities for", model))
+ggsave(paste0(results_dir, model, "-densities.pdf"), densities_plot, device = cairo_pdf)
 
 
-chains_plot_gmm <- thin_chains %>%
+chains_plot <-
+    thin_chains %>%
     ## arrange(particles) %>%
     ## mutate(algorithm = as_factor(str_c(discrete_algorithm, " & ", continuous_algorithm,
                                        ## ifelse(discrete_algorithm == "PG",
@@ -120,7 +126,8 @@ acfs <- chains_by_param %>%
     })
     
 
-acf_plot_gmm <- acfs %>%
+acf_plot <-
+    acfs %>%
     ggplot(aes(x = lag, y = acf)) +
     geom_linerange(aes(ymin = 0, ymax = acf)) +
     facet_grid(parameter ~ data_size,
@@ -130,30 +137,28 @@ acf_plot_gmm <- acfs %>%
     geom_hline(aes(yintercept = -significance_level), lty=3,
                data = significance_levels) +
     labs(x = 'Lag', y = 'ACF',
-         title = "Autocorrelation",
-         subtitle = "ACF plots for one sample chain per data size")
-ggsave("results/GMM-acfs.pdf", acf_plot_gmm, device = cairo_pdf)
+         title = paste("Autocorrelation estimate for", model))
+ggsave(paste0(results_dir, model, "-acfs.pdf"), acf_plot, device = cairo_pdf)
 
 
 
 
 ## filter(compile_times, is_first == F) %>%
-compiletime_plot_gmm <- compile_times %>%
+compiletime_plot <-
+    compile_times %>%
     ggplot(aes(x = data_size,
                y = compilation_time,
-               shape = as_factor(ifelse(is_first, "First", "Other")))) +
-    geom_point(alpha = 0.5, size = 3) +
+               color = as_factor(ifelse(is_first, "First", "Other")))) +
+    geom_point(size = 3) +
     geom_smooth(formula = y ~ I(x^2), method = "lm", se = F,
                 data = filter(compile_times, is_first == F),
                 color = "black") +
-    annotate("text", x = 85, y = 100,
-             label = expression("Linear fit of" ~ time %~% datasize^2),
-             family = base_font) + 
+    ## annotate("text", x = 40, y = 15,
+    ##          label = expression("Linear fit of" ~ time %~% datasize^2),
+    ##          family = base_font) + 
     ## scale_x_continuous(breaks = unique(compile_times$data_size)) +
     labs(x = "Observations (data size)", y = "Extraction time (s)",
          shape = "Repetition",
-         title = "Extraction times for AutoGibbs",
-         subtitle = paste(
-             "Measuring both compilation of the traced code and the conditional calculation.",
-             "All 2 or 3 repetitions per data size class are shown."))
-ggsave("results/GMM-compile_times.pdf", compiletime_plot_gmm, device = cairo_pdf)
+         color = "Repetition",
+         title = paste("AutoGibbs extraction times for", model))
+ggsave(paste0(results_dir, model, "-compile_times.pdf"), compiletime_plot, device = cairo_pdf)
