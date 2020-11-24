54d4eb3bf010f2f12ef79ac30e821e94531a007b
Philipp Gabler <phipsgabler@users.noreply.github.com>
2020-11-24 12:48:22 +0100

    Nicer code for tuning script

--- test/tuning.jl
+++ test/tuning.jl
@@ -16,34 +16,56 @@ const MODEL_SETUPS = Dict([
 ])
 const DATA_SIZE = 10
 
-function evaluate(model::String, nuts::String, stepsize::String, steps::String, particles::String)
+function evaluate(model::String, samplers::String, stepsize::String, steps::String, particles::String)
     return evaluate(
         model,
-        parse(Bool, nuts),
+        samplers,
         parse(Float64, stepsize),
         parse(Int, steps),
         parse(Int, particles)
     )
 end
 
-function evaluate(model, nuts, stepsize, steps, particles)
+"""
+    evaluate(model, samplers, stepsize, steps, particles)
+
+Try out the model with specified samplers and parameters.  `stepsize` and `steps` are for HMC,
+`particles` is for PG.  `samplers` is a string containing either `nuts` or `hmc`, and either
+`ag` or `pg`.  Unneeded parameters are ignored, but must be present for the signature.
+"""
+function evaluate(model, samplers, stepsize, steps, particles)
     generate, example, tarray_example, p_discrete, p_continuous = MODEL_SETUPS[model]
 
     dataname, data = generate(DATA_RNG, DATA_SIZE)
     model_pg = tarray_example(; dataname => data)
 
-    if nuts
+    if occursin(r"nuts"i, samplers)
+        println("NUTS for continuous sampler.")
         dummy_nuts = NUTS()
         ad, _, metric = typeof(dummy_nuts).parameters
-        sampler = NUTS{ad, (p_continuous...,), metric}(
-            map(f -> getfield(dummy_nuts, f), fieldnames(typeof(n)))...
+        sampler_continuous = NUTS{ad, (p_continuous...,), metric}(
+            map(f -> getfield(dummy_nuts, f), fieldnames(typeof(dummy_nuts)))...
         )
+    elseif occursin(r"hmc"i, samplers)
+        println("HMC for continuous sampler.")
+        sampler_continuous = HMC(stepsize, steps, p_continuous...)
     else
-        sampler = HMC(stepsize, steps, p_continuous...)
+        error("Unknown continuous sampler")
+    end
+    
+    if occursin(r"ag"i, samplers)
+        println("AG for discrete sampler. Compiling static conditional...")
+        sampler_discrete = StaticConditional(model_pg, p_discrete)
+        println("Done.")
+    elseif occursin(r"pg"i, samplers)
+        println("PG for discrete sampler.")
+        sampler_discrete = PG(particles, p_discrete)
+    else
+        error("Unknown discrete sampler")
     end
     
     @show data
-    chain = sample(model_pg, Gibbs(sampler, PG(particles, p_discrete)), 1000)
+    chain = sample(model_pg, Gibbs(sampler_continuous, sampler_discrete), 1000)
     show(chain)
     plot(chain)
 end
